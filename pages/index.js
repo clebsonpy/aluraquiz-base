35c6a9136828be04234668b4ff84dae0eebe408e
Clebson Farias <clebson.farias@ctec.ufal.br>
2021-01-26 18:40:21 -0300

    Aluna 2

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,13 @@
-import styled from 'styled-components'
-import db from '../db.json'
-import Widget from '../src/components/Widget'
-import GitHubCorner from '../src/components/GitHubCorner'
-import Footer from '../src/components/Footer'
-import QuizBackground from '../src/components/QuizBackground'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
-// const BackgroundImage = styled.div`
-//   background-image: url(${db.bg});
-//   flex: 1;
-//   background-size: cover;
-//   background-position: center;
-// `
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import GitHubCorner from '../src/components/GitHubCorner';
+import Footer from '../src/components/Footer';
+import QuizBackground from '../src/components/QuizBackground';
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -21,25 +18,44 @@ export const QuizContainer = styled.div`
     margin: auto;
     padding: 15px;
   }
-`
-
+`;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>AluraQuiz - Modelo Base</title>
+      </Head>
       <QuizContainer>
         <Widget>
           <Widget.Header>
-              <h1>The legend of zelda</h1>
-            </Widget.Header>
+            <h1>The legend of zelda</h1>
+          </Widget.Header>
           <Widget.Content>
-            <p>Lorem ipsum doior amet....</p>
+            <form onSubmit={function (e) {
+              e.preventDefault();
+              router.push(`quiz?name=${name}`);
+            }}
+            >
+              <input
+                onChange={function (infosDoEvento) {
+                  setName(infosDoEvento.target.value);
+                }}
+                placeholder=""
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+                {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
         <Widget>
           <Widget.Content>
             <h1>Quizes da Galera</h1>
-
             <p>Lorem ipsum doior amet....</p>
           </Widget.Content>
         </Widget>
@@ -47,5 +63,5 @@ export default function Home() {
       </QuizContainer>
       <GitHubCorner projectUrl="https://github.com/clebsonpy" />
     </QuizBackground>
-  )
+  );
 }
